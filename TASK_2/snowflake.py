17fec0dffb64c430b5f49a90d0cca57d4a58d037
Alexander <defakto.gylop@gmail.com>
2025-01-12 22:13:49 +0200

    result

---
+++ TASK_2/snowflake.py
@@ -0,0 +1,31 @@
+import turtle
+k = int(input ("Ведіть рівень рекурсії: ")) 
+
+def koch_curve(t, order, size):
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_curve(t, order - 1, size / 3)
+            t.left(angle)
+
+def draw_koch_curve(order, size=300):
+    window = turtle.Screen()
+    window.bgcolor("white")
+
+    t = turtle.Turtle()
+    t.speed(0)  
+    t.penup()
+    t.goto(-size / 2, 0)
+    t.pendown()
+    
+    for _ in range(k):
+        koch_curve(t, order, size)
+        t.right(120)
+
+    koch_curve(t, order, size)
+
+    window.mainloop()
+
+# Виклик функції
+draw_koch_curve(k)
